86cf6bfb7b84bf948e0f670c74bfb5c64d2c2c02
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-11-10 17:10:07 +0500

    working

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,11 +1,27 @@
-import StyledProfile from "./style"
+import { Avatar } from "antd";
+import StyledProfile from "./style";
+import { useAppSelector } from "hooks";
 
 const UserProfile = () => {
-    return (
-        <StyledProfile>
-            <h1>My profile</h1>
-        </StyledProfile>
-    )
-}
+  const user = useAppSelector(({ auth }) => auth.user);
+  return (
+    <StyledProfile>
+      <div className="profile__user">
+        <div className="user__avatar">
+          <Avatar
+            className="avatar__img"
+            src="https://api.dicebear.com/7.x/fun-emoji/svg?eyes=closed&mouth=lilSmile"
+            size={{ xs: 24, sm: 32, md: 40, lg: 60, xl: 70, xxl: 80 }}
+          />
+          <div>
+            <div className="user__name">{user?.name}</div>
+            <div className="info__detail">+{user?.phone}</div>
+          </div>
+        </div>
+      </div>
+      <div className="profile__content"></div>
+    </StyledProfile>
+  );
+};
 
-export default UserProfile
+export default UserProfile;
--- src/pages/User/style.ts
+++ src/pages/User/style.ts
@@ -1,7 +1,38 @@
+import { colors } from "constants/styles";
 import styled from "styled-components";
+import { layout } from "styles/mixin";
 import { pxToRem } from "utils";
 
+const { gray } = colors;
 
 export default styled.div`
-    padding: ${pxToRem(50)} 0;
-`
+  ${layout("flex")}
+  align-items: flex-start;
+  .profile__user {
+    width: 100%;
+    max-width: ${pxToRem(350)};
+    min-width: ${pxToRem(350)};
+    height: calc(100vh - ${pxToRem(80)});
+    position: sticky;
+    top: ${pxToRem(80)};
+    padding: ${pxToRem(20)} 0;
+    border-right: ${pxToRem(1)} solid ${gray}30;
+  }
+  .user__avatar {
+    ${layout("flex")}
+    justify-content: start;
+    div {
+      margin-left: ${pxToRem(10)};
+    }
+  }
+  .user__name {
+    font-size: ${pxToRem(30)};
+    font-weight: 600;
+  }
+  .avatar__img {
+    border: ${pxToRem(1)} solid ${gray}80;
+  }
+  .profile__content {
+    width: 100%;
+  }
+`;
