0e675f87f23ab8c9d526691657384ab2fb5867b9
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-11-12 14:57:36 +0500

    working

--- src/components/Text/style.ts
+++ src/components/Text/style.ts
@@ -1,5 +1,17 @@
 import styled from "styled-components";
+import { pxToRem } from "utils";
 
 export const StyledH1 = styled.div<{
   $size: "sm" | "md" | "lg" | "xl" | "xxl";
-}>``;
+}>`
+  ${({ $size }) =>
+    $size === "sm"
+      ? `font-size: ${pxToRem(16)}`
+      : $size === "md"
+      ? `font-size: ${pxToRem(20)}`
+      : $size === "lg"
+      ? `font-size: ${pxToRem(30)}`
+      : $size === "xl"
+      ? `font-size: ${pxToRem(35)}`
+      : `font-size: ${pxToRem(40)}`}
+`;
