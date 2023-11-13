57e972cd7a2a6a8d23559b96ac9f3f8a4f7addc6
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-11-13 10:39:11 +0500

    working

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { compression } from "vite-plugin-compression2";
-import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), compression(), svgr()],
+  plugins: [react(), tsconfigPaths(), compression()],
   resolve: { alias: [{ find: "@", replacement: "/src" }] },
   base: "./",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
 });
