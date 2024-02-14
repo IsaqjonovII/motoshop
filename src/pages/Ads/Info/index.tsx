2a7c2e8370ced306103c45ed04313f32d64ae45f
IsaqjonovII <ilhomjonisoqjonov2@gmail.com>
2024-02-14 20:06:19 +0500

    ad info page started

--- src/pages/Ads/Info/index.tsx
+++ src/pages/Ads/Info/index.tsx
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { FiEye } from "react-icons/fi";
+import { IoMdHeart } from "react-icons/io";
+import { formatNumbers } from "utils";
 import { useGetAdByIdQuery } from "services/ad";
-import { IAdHelmetAndGear, IAdMoto } from "interfaces/responses";
+import { IAdHelmetAndGear, IAdMoto, IMotoAd } from "interfaces/responses";
 import StyledAdInfo from "./style";
+import { Text } from "components/Text";
+import { Button } from "components/Button";
 
 type TParams = {
   id: string;
@@ -10,7 +16,8 @@ type TParams = {
 const AdInfo = () => {
   const { id } = useParams<TParams>();
   const [adData, setadData] = useState<IAdMoto | IAdHelmetAndGear>();
-  const { data, isLoading, error, isError, refetch } = useGetAdByIdQuery(id!);
+  const { data, isLoading, error, refetch } = useGetAdByIdQuery(id!);
+  console.log(isLoading, error);
 
   useEffect(() => {
     refetch();
@@ -24,13 +31,109 @@ const AdInfo = () => {
   return (
     <StyledAdInfo>
       <div className="page__container">
-        <div className="images">
-        </div>
+        <div className="images"></div>
 
         <div className="content">
-          <div className="content__head"></div>
+          <div className="content__head">
+            <Text size="xl" bold={600}>
+              {adData?.title}{" "}
+            </Text>
+            <br />
+            <div className="head__items">
+              <Text size="sm" bold={300}>
+                <FiEye className="icon" /> {adData?.views} marta ko'rilgan
+              </Text>
+              <Text size="sm" bold={300}>
+                <IoMdHeart className="icon" />
+                {adData?.likes} kishi yoqtirgan
+              </Text>
+            </div>
+          </div>
 
-          <div className="content__body"></div>
+          <div className="content__body">
+            <Text size="xxl" bold={600}>
+              {adData?.price.currency === "usd"
+                ? `$${formatNumbers(parseInt(adData.price.amount))}`
+                : adData?.price.currency === "uzs"
+                ? `${formatNumbers(parseInt(adData.price.amount))} so'm`
+                : `€${formatNumbers(parseInt(adData?.price.amount ?? ""))}`}
+            </Text>
+            <br />
+            <div className="owner__contact">
+              <Link to={"tel:" + adData?.owner.phone}>
+                <Button>Qo&apos;ng&apos;iroq qilish</Button>
+              </Link>
+            </div>
+            <br />
+            <ul>
+              <li className="body__list">
+                {adData?.adType === "moto"
+                  ? "Ishlab chiqarilgan yili:"
+                  : adData?.adType === "gear"
+                  ? "O'lchami"
+                  : null}{" "}
+                <Text size="md" bold={600}>
+                  {adData?.adType === "moto"
+                    ? (adData as IMotoAd).manufacturedAt
+                    : adData?.adType === "gear"
+                    ? (adData as IAdHelmetAndGear).size
+                    : null}
+                </Text>
+              </li>
+              <li className="body__list">
+                {adData?.adType === "moto"
+                  ? "Bosgan masofasi: "
+                  : adData?.adType === "gear"
+                  ? "Holati:  "
+                  : null}{" "}
+                <Text size="md" bold={600}>
+                  {adData?.adType === "moto"
+                    ? (adData as IMotoAd).mileage + " km"
+                    : adData?.adType === "gear"
+                    ? (adData as IAdHelmetAndGear).condition
+                    : null}
+                </Text>
+              </li>
+              <li className="body__list">
+                {adData?.adType === "moto"
+                  ? "Turi: "
+                  : adData?.adType === "gear"
+                  ? "Brand:  "
+                  : null}{" "}
+                <Text size="md" bold={600}>
+                  {adData?.adType === "moto"
+                    ? (adData as IMotoAd).category
+                    : adData?.adType === "gear"
+                    ? (adData as IAdHelmetAndGear).brand
+                    : null}
+                </Text>
+              </li>
+              <li className="body__list">
+                {adData?.adType === "moto"
+                  ? "Dvigatel hajmi: "
+                  : adData?.adType === "gear"
+                  ? "Rangi:  "
+                  : null}{" "}
+                <Text size="md" bold={600}>
+                  {adData?.adType === "moto"
+                    ? (adData as IMotoAd).engineSize + " cc"
+                    : adData?.adType === "gear"
+                    ? (adData as IAdHelmetAndGear).color
+                    : null}
+                </Text>
+              </li>
+              <li className="body__list">
+                Rangi:{" "}
+                <Text size="md" bold={600}>
+                  {adData?.color}
+                </Text>
+              </li>
+            </ul>
+            <br />
+            <Text size="md" bold={400}>
+              {adData?.description}
+            </Text>
+          </div>
         </div>
       </div>
     </StyledAdInfo>
--- src/pages/Ads/Info/style.ts
+++ src/pages/Ads/Info/style.ts
@@ -1,10 +1,12 @@
 import { colors } from "constants/styles";
 import styled from "styled-components";
 import { layout } from "styles/mixin";
+import { pxToRem } from "utils";
 
-const { teal, gray, borderGray } = colors;
+const { gray } = colors;
 
 export default styled.div`
+  padding: ${pxToRem(50)} 0;
   .page__container {
     min-height: 60vh;
     min-height: 60svh;
@@ -12,12 +14,27 @@ export default styled.div`
   }
   .images {
     grid-column: 1 / 3;
-    background-color: red;
+    border-right: 1px solid ${gray}30;
+    padding-right: ${pxToRem(20)};
   }
   .content {
     grid-column: 3 / 4;
-    background-color: green;
     position: sticky;
     top: 0;
+    padding-left: ${pxToRem(20)};
+  }
+  .head__items {
+    ${layout("flex")};
+    flex-wrap: wrap;
+  }
+  .content__head {
+    border-bottom: ${pxToRem(1)} solid ${gray}30;
+    padding-bottom: ${pxToRem(10)};
+  }
+  .content__body {
+    padding: ${pxToRem(10)} 0;
+  }
+  .body__list {
+    ${layout("flex")}
   }
 `;
