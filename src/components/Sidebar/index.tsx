9d1ac1f1ab60dbec279ab86f2758bdd6438945ae
IsaqjonovII <ilhomjonisoqjonov2@gmail.com>
2024-04-07 16:37:52 +0500

    upcoming new features

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,24 +1,24 @@
-import { Link } from "react-router-dom";
-import { RightOutlined } from "@ant-design/icons";
-import { navRoutes } from "routes";
-import StyledSidebar from "./style";
-import { ISidebar } from "interfaces";
+// import { Link } from "react-router-dom";
+// import { RightOutlined } from "@ant-design/icons";
+// import { navRoutes } from "routes";
+// import StyledSidebar from "./style";
+// import { ISidebar } from "interfaces";
 
-const Sidebar = ({ isSidebarOpen }: ISidebar) => {
-  return (
-    <StyledSidebar
-      className={isSidebarOpen ? "sidebar sidebar__opened" : "sidebar"}
-    >
-      <div className="sidebar__links">
-        {navRoutes.map(({ key, path }) => (
-          <div key={key} className="sidebar__link">
-            <Link to={path}>{key}</Link>
-            <RightOutlined />
-          </div>
-        ))}
-      </div>
-    </StyledSidebar>
-  );
-};
+// const Sidebar = ({ isSidebarOpen }: ISidebar) => {
+//   return (
+//     <StyledSidebar
+//       className={isSidebarOpen ? "sidebar sidebar__opened" : "sidebar"}
+//     >
+//       <div className="sidebar__links">
+//         {navRoutes.map(({ key, path }) => (
+//           <div key={key} className="sidebar__link">
+//             <Link to={path}>{key}</Link>
+//             <RightOutlined />
+//           </div>
+//         ))}
+//       </div>
+//     </StyledSidebar>
+//   );
+// };
 
-export default Sidebar;
+// export default Sidebar;
